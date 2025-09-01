cf9fb46e7426c86f5523531a046e0635eb04f2f0
pixelbymoz <pixelbymoz@gmail.com>
2025-09-01 23:29:02 +0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import App from './App.tsx';
 import './index.css';
 
